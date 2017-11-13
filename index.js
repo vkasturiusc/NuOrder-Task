e0665f5a13ee191c6fa35dd0673c49bbed63ce87
vkasturiusc <vkasturi@usc.edu>
2017-11-13 01:39:56 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
+'use strict'
 console.log('Initial Release')
+
+var a = {
+    first_name: 'Bob',
+    last_name: 'Joness',
+    email: 'bob@gmail.com',
+    address: {
+        line_1: '1234 Main St',
+        line_2: 'Apt 413',
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: '90048'
+    },
+    logins: [
+            {date:'10/22/2012',ip:'192.168.0.1'},
+            {date:'10/21/2012',ip:'192.168.0.1'}
+    ],
+    photos: [
+        'IMG-1985.jpg',
+        'IMG-1987.jpg'
+    ]
+}
+
+var b = {
+    last_name: 'Jones',
+    active: true,
+    address: {
+        line_1: '2143 South Main St',
+        line_2: undefined
+    },
+    logins: {date: '10/23/2012',ip:'192.168.0.1'},
+    photos: undefined 
+
+}
+function merger(item,a,b){
+    
+    if(Array.isArray(a[item])){
+
+        if(b[item] !== undefined && (!Array.isArray(b[item]))){
+            a[item].push(b[item]);
+        }
+        else if(b[item] === undefined){
+           
+            a[item] = [];
+        }
+        else if(Array.isArray(b[item])){
+            a[item] = b[item];
+        }
+    }
+    else if(b[item] && typeof b[item] !== 'object'){
+        a[item] = b[item];
+
+    }
+    else if(a[item] && b[item] === undefined){
+        delete a[item];
+    }
+    else{
+        for(var innerItem in b[item]){
+            
+            if(b[item].hasOwnProperty(innerItem)){
+            
+                merger(innerItem,a[item],b[item]);
+            }
+        }
+    }
+    return true;
+};
+
+Object.keys(b).forEach(function(key){
+    merger(key,a,b);
+});
+console.log(a);
