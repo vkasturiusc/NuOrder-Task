f212fed7840c996a2bbaace1a42b832c6db7eea1
vkasturiusc <vkasturi@usc.edu>
2017-11-13 21:43:34 -0800

    modified test cases

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
 'use strict'
 
 function merger(item,a,b){
-    console.log('***********************');
-    console.log('From A: \n'+item+':'+JSON.stringify(a[item]));
-    console.log('-----------------------');
-    console.log('From B: \n'+item+':'+JSON.stringify(b[item]));
-    console.log('***********************');
+   
     if(Array.isArray(a[item])){
 
         if(b[item] !== undefined && (!Array.isArray(b[item]))){
-            console.log('\nCondition 1\n');
+            //Condition 1
             a[item].push(b[item]);
         }
         else if(b[item] === undefined || b[item] === null){
-            console.log('\nCondition 2\n');
+            //Condition 2
             a[item] = [];
         }
         else if(Array.isArray(b[item])){
-            console.log('\nCondition 3\n');
+            //Condition 3
             a[item] = b[item];
         }
     }
     else if(a.hasOwnProperty(item) && b[item] === undefined){
-        console.log('\nCondition 4\n');
+        //Condition 4
         delete a[item];
     }
     else if(b[item] && typeof b[item] !== 'object'){
-        console.log('\nCondition 5\n');
+        //Condition 5
         a[item] = b[item];
 
     }
     else if(a[item] && Array.isArray(b[item])){
-        console.log('\nMy Condition\n');
+        // Condition as discussed in e-mail
         a[item] = b[item];
     }
     else{
-        for(var innerItem in b[item]){
-            
-            if(b[item].hasOwnProperty(innerItem)){
-            
-                merger(innerItem,a[item],b[item]);
+        if(a[item] !== undefined){
+            for(var innerItem in b[item]){  
+                if(b[item].hasOwnProperty(innerItem)){
+                    merger(innerItem,a[item],b[item]);
+                }
             }
         }
+        
     }
-    console.log('Result: \n'+item+':'+JSON.stringify(a[item]));
     return true;
 };
 
@@ -54,5 +50,3 @@ module.exports.mergewrapper = function mergewrapper(a,b){
     return a;
 }
 
-
-//console.log(mergewrapper(x,y));
--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var index = require('../index');
 
-var x = {
+const x = {
     first_name: 'Bob',
     last_name: 'Joness',
     email: 'bob@gmail.com',
@@ -18,11 +18,34 @@ var x = {
             {date:'10/22/2012',ip:'192.168.0.1'},
             {date:'10/21/2012',ip:'192.168.0.1'}
     ],
-    photos: 'IMG-1985.jpg'
+    photos: ['IMG-1985.jpg',
+             'IMG-1987.jpg'
+    ]
         
 }
 
-var y = {
+const x1 = {
+    first_name: 'Bob',
+    last_name: 'Joness',
+    email: 'bob@gmail.com',
+    address: {
+        line_1: '1234 Main St',
+        line_2: 'Apt 413',
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: '90048'
+    },
+    logins: [
+            {date:'10/22/2012',ip:'192.168.0.1'},
+            {date:'10/21/2012',ip:'192.168.0.1'}
+    ],
+    photos: ['IMG-1985.jpg',
+             'IMG-1987.jpg'
+    ]
+        
+}
+
+const y = {
     last_name: 'Jones',
     active: true,
     address: {
@@ -34,7 +57,19 @@ var y = {
 
 }
 
-var result_stub = {
+const y1 = {
+    last_name: 'Jones',
+    active: true,
+    address: {
+        line_1: '2143 South Main St',
+        line_2: undefined
+    },
+    logins: {date: '10/23/2012',ip:'192.168.0.1'},
+    photos: undefined 
+
+}
+
+var result_stub_1 = {
     first_name: 'Bob',
     last_name: 'Jones',
     active: true,
@@ -46,12 +81,31 @@ var result_stub = {
     zip: '90048'
     },
     logins: [
-    { date: '10/22/2012', ip: '192.168.0.1' },
-    { date: '10/21/2012', ip: '192.168.0.1' },
-    { date: '10/23/2012', ip: '192.168.0.1' }
+        { date: '10/22/2012', ip: '192.168.0.1' },
+        { date: '10/21/2012', ip: '192.168.0.1' },
+        { date: '10/23/2012', ip: '192.168.0.1' }
     ],
     photos: []
     } 
+
+var result_stub_2 = { 
+    last_name: 'Joness',
+    active: true,
+    address: { 
+        line_1: '1234 Main St',
+        line_2: 'Apt 413',
+        city: 'Los Angeles',
+        state: 'CA',
+        zip: '90048' 
+    },
+    logins: [ 
+        { date: '10/22/2012', ip: '192.168.0.1' },
+        { date: '10/21/2012', ip: '192.168.0.1' } 
+    ],
+    photos: undefined,
+    first_name: 'Bob',
+    email: 'bob@gmail.com' 
+}
 
 
 
@@ -64,28 +118,25 @@ describe('Testing Object Merger',function(){
         }catch(e){
             done(e);
         }
-
     });
     it('Sample test case B to A', function(done){
         try{
-            let result = index.mergewrapper(x,y);
-            assert.deepEqual(result_stub,result);
-            done();  
-        }catch(e){
-            done(e);
-        }
 
-    });
-    it.skip('Sample test case A to B', function(done){
-        try{
-            let result = index.mergewrapper(y,x);
-            console.log('\n&&&&&&&&&&&&&&&&&&&&');
-            console.log(result);
-            console.log('\n&&&&&&&&&&&&&&&&&&&&\n');
-            assert.deepEqual(result_stub,result);
+            let result = index.mergewrapper(x,y);
+            assert.deepEqual(result_stub_1,result);
             done();  
         }catch(e){
             done(e);
         }
     });
+    it('Sample test case A to B', function(done){
+        try{
+            let result = index.mergewrapper(y1,x1);
+            assert.deepEqual(result_stub_2,result);
+            done();  
+        }catch(e){
+            done(e);
+        }
+    });
+
 });
